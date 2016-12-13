e48b2a63eae3c25e6c6bd1d6fe16da639f70ffa5
sphsconormurphy <8cmurphy@stpatrick.org>
2016-12-13 13:54:04 -0600

    Day 2

--- First-Semester-Final-master/password.js
+++ First-Semester-Final-master/password.js
@@ -1,11 +1,34 @@
 class Password{
   //The first part of the class.
-
+  constructor(p, pk){
+    this.publickey = p;
+    this.privatekey = pk;
+  }
 
 
   //Instance functions below this comment.
-
-
+  validPublicKey(){
+    if(this.publickey.length >= 8 && this.publickey.length <= 25){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  validPrivateKey(){
+    if(this.privatekey[4] == "-" && this.privatekey[9] == "-"){
+      if(){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    else{
+      return false;
+    }
+  }
 
   //Static function below this comment.
+
 }
